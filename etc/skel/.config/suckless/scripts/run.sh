67a96b6349b5bd56c482036fb4be14f0d500f23e
Ilan Shavit <ilan.shavit@gmail.com>
2025-09-14 07:44:43 +0300

    Updates

--- etc/skel/.config/suckless/scripts/run.sh
+++ etc/skel/.config/suckless/scripts/run.sh
@@ -6,31 +6,28 @@ function run {
     $@&
   fi
 }
-run "alacritty -e xrandr --output DP1 --mode 1920x1080"
+# run "alacritty -e xrandr --output DP1 --mode 1920x1080"
+/home/ilan/.screenlayout/nuc-resolution.sh
+
 run "nm-applet"
-# run "pamac-tray"
 run "variety -n"
 run "xfce4-power-manager"
 run "xfce4-clipman"
 run "blueberry-tray"
 run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-
 run pa-applet
 run "numlockx on"
+# run "pamac-tray"
 
+#  Personal Apps
 run sxhkd -c ~/.config/suckless/sxhkd/sxhkdrc &
-
-
-###  Personal Apps ###
 run /usr/bin/synology-drive &
-#run setxkbmap -layout "us,il" -option "grp:alt_shift_toggle"
-run redshift-gtk -l 31.046051:34.851612 &
 run /home/ilan/DATA/SynologyDrive/SOFTWARE/Linux/AppImage/Nextcloud-3.17.1-x86_64.AppImage &
-
-run picom -b  --config ~/.config/suckless/picom/picom.conf &
 run /home/ilan/.bin/keyboard_tray.py &
-
+run redshift-gtk -l 31.046051:34.851612 &
+run picom -b  --config ~/.config/suckless/picom/picom.conf &
+#run setxkbmap -layout "us,il" -option "grp:alt_shift_toggle"
 
 pkill bar.sh
 ~/.config/suckless/scripts/bar.sh &
